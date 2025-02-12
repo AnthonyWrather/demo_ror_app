930ecc8c6c44a1f0b03c2744d9b44d129aa758e1
Anthony Wrather <github@anthonywrather.me.uk>
2025-02-12 02:33:41 +0000

    Updating the tests.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,7 +2,12 @@ require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get home_index_url
+    get :home
+    assert_response :success
+  end
+
+  test "should get index too" do
+    get static_pages_home_url
     assert_response :success
   end
 end
